26d6b6ece3c92508a2571bd551f31ca66bf98852
Muhamad Daffa <daffafa09@gmail.com>
2024-12-09 07:50:20 +0700

    feat: add category page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var categoryRouter = require("./app/category/router");
+var dashboardRouter = require("./app/dashboard/router");
 
 var app = express();
 
@@ -26,7 +27,8 @@ app.use(
   express.static(path.join(__dirname, "node_modules/admin-lte/plugins"))
 );
 
-app.use("/", categoryRouter);
+app.use("/category", categoryRouter);
+app.use("/", dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
